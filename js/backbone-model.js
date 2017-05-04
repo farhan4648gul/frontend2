5f9d01be6e54ef78789aa7182cb53a7d2a8f8874
Farhan Gul <farhan4648gul@gmail.com>
2017-05-04 20:05:43 +0500

    Day end commit

---
+++ js/backbone-model.js
@@ -0,0 +1,63 @@
+
+// Default Model for Application
+
+var ModuleModel = Backbone.Model.extend({
+    defaults: {
+        // title: '', //models in my case do not need a title, collections do
+        url: '/',
+        // model_type: '', //type in collection, based on user_role
+
+        //attributes list
+        id: '',
+        email: '',
+        username: '',
+        first_name: '',
+        last_name: '',
+        middle_name: '',
+        phone: '',
+        address: '',
+        user_role: '',
+        university_id: '',
+        added_on: '',
+        updated_on: '',
+        updated_by: '',
+        status: '',
+        blocked: '',
+
+    },
+    idAttribute: 'id', // will it work? //based on primary id
+    initialize: function(){
+
+        //where to bind error show event ?
+
+        //fill dummy data
+        this.demoData();
+        console.log('data filled');
+    },
+
+    validate: function(){
+        // ? dummy data should be validated
+    },
+
+    demoData: function(){
+
+        this.attributes.id = Math.floor( Math.random() * 100000 );
+        this.attributes.email = Math.floor( Math.random() * 100000 );
+        this.attributes.username = Math.floor( Math.random() * 100000 );
+        this.attributes.first_name = Math.floor( Math.random() * 100000 );
+        this.attributes.last_name = Math.floor( Math.random() * 100000 );
+        this.attributes.middle_name = Math.floor( Math.random() * 100000 );
+        this.attributes.phone = Math.floor( Math.random() * 100000 );
+        this.attributes.address = Math.floor( Math.random() * 100000 );
+        this.attributes.user_role = Math.floor( Math.random() * 100000 );
+        this.attributes.university_id = Math.floor( Math.random() * 100000 );
+        this.attributes.added_on = Math.floor( Math.random() * 100000 );
+        this.attributes.updated_on = Math.floor( Math.random() * 100000 );
+        this.attributes.updated_by = Math.floor( Math.random() * 100000 );
+        this.attributes.status = Math.floor( Math.random() * 100000 );
+        this.attributes.blocked = Math.floor( Math.random() * 100000 );
+
+    },
+    urlRoot: window.location.href + '/' + this.model_type + '/', //change this with app.webroot
+
+});
--- js/main.js
+++ js/main.js
@@ -2,105 +2,23 @@
 * Created by farhan.gul on 5/3/17.
 */
 
- window.App = {
+
+
+//namespace app
+ window.App1 = {
    Models: {},
    Collections: {},
    Views: {},
+   Templates: {},
 
 };
 
-var app = {}; //should be window.App
-var View = Backbone.View.extend({
 
-    el: '#main-content',
-    template: $("#main-content").html(),
 
-    initialize: function(){
-        this.render();
-    },
-
-    render: function(){
-        //should I put code in initialize or just here?
-        this.showNavigation();
-        this.showDashboard();
-    },
-
-    doRender: function(elem, template, content){
-        elem = $("#"+elem);
-        template = $("#"+template);
-        var templateScript = Handlebars.compile(template.html());
-        var html = templateScript(content);
-        elem.html(html);
-
-    },
-
-    showDashboard: function(){
-
-        var content = [];
-        content['firstname'] = "Admin";
-        content['lastname'] = "User"; //get this data from authorization api
-
-        this.doRender("content-panel", "content-default", content);
-    },
-
-    showNavigation: function(){
-
-        var content = [];
-        content['title'] = 'Navigation';
-        content['links'] = app.router.routes;
-
-        this.doRender("sidebar-nav", "main-nav", content);
-
-    }
-
-});
-
-var Router = Backbone.Router.extend({
-    routes: {
-        "" : "home", //using class active throws error
-        "me": "validateSession",
-        "course": "course",
-        "student": "student",
-        "teacher": "teacher",
-    },
-    initialize: function(){
-        //this.bind('route', this.changeRoute);  // triggers after 'route' is finished
-    },
-    home: function(){
-        console.log('in home function');
-        app.baseView.showDashboard();
-    },
-    validateSession: function(){
-        // TODO: create authentication
-        console.log('validating user session');
-    },
-    clearPageArea: function(){
-        $("#content-panel").html('');
-    },
-    changeRoute: function(){
-        console.log('clearing');
-        this.clearPageArea();
-    },
-    execute: function(callback, args, name){
-        this.executeBefore();
-        if ( callback ) callback.apply(this, args);
-    },
-    executeBefore: function(){
-        // console.log('finally');
-        this.clearPageArea();
-    },
-    selectedMenu: function(menuId){
-        $("#"+menuId).addClass('active');
-        // console.log('added');
-    },
-    clearSelectedMenu: function(){
-        $("#menu-items").removeClass('active');
-    }
-
-});
 
 app.router = new Router();
 app.baseView = new View();
+app.baseView.render();
 
 Backbone.history.start();
 Backbone.history.on('route', function(){
@@ -116,10 +34,26 @@ Backbone.history.on('route', function(){
 //authenticate user
 
 
-//Handlebars functions
-Handlebars.registerHelper('uppercase', function(context, options){
-    //what is options here?
-    console.log('inside');
-    return context.charAt(0).toUpperCase() + context.slice(1);
+// create dummy collection to fill moduleview
+// app.studentModel = new ModuleModel();
+// app.studentModel2 = new ModuleModel(); //only 1 record is showing, why?
 
-});
+ // app.studentCollection = new ModuleCollection( [app.studentModel, app.studentModel2]);
+
+/*app.studentCollection = new ModuleCollection();
+app.studentCollection.add(app.studentModel);
+app.studentCollection.add(app.studentModel2);*/
+
+// app.studentCollection.meta('type', 'student');
+// app.studentCollection.meta('title', 'Student');
+// app.studentCollection.meta('description', 'A little description here');
+
+
+// console.log('student:');
+// console.log(app.studentCollection);
+/*for (var v in app.studentModel.attributes){
+    console.log('attribute: ' + v);
+}*/
+
+
+// app.ModuleView = new ModuleView(app.studentCollection);
